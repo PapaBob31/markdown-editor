78bc03b5e549d7a791080cb9685ab8021155cc83
Adedamola Egbebi <egbebitimi121@gmail.com>
2024-05-28 12:39:44 +0100

    Added general syntax page

---
+++ src/app/syntax/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Page() {
+  return (
+    <>
+      <header>
+        <h1>General Syntax</h1>
+      </header>
+      <main>
+        <i>Work in progress...</i>
+      </main>
+    </>
+  )
+}
