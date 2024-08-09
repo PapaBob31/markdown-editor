309d147b4866668837f4afaf77a1ac62e653f89a
Adedamola Egbebi <egbebitimi121@gmail.com>
2024-08-09 20:39:05 +0100

    mostly implemented the feature of parsing links

--- src/app/[slug]/parser.tsx
+++ src/app/[slug]/parser.tsx
@@ -1,6 +1,6 @@
 import type {HtmlNode} from "./page"
 import {validListChild, getInnerMostOpenContainer, getValidOpenedAncestor, closeNode, checkIfPartOfOtherNodeTypes} from "./utilities"
-import {getBlockNodes, parseInlineNodes} from "./parserHelper"
+import {getBlockNodes} from "./parserHelper"
 
 function determineProperParentNode(lastOpenedNode: HtmlNode, nodeName: string, line: string, markerPos: number) {
 	if (nodeName === "blockquote" && lastOpenedNode.indentLevel as number - markerPos < 0) {
@@ -29,7 +29,6 @@ function getHeaderNodeObj(line: string, lastOpenedNode: HtmlNode): HtmlNode {
 	let headerDetails = line.match(/(\s*)(#+)\s/) as RegExpMatchArray;
 	let ph = headerDetails[1].length;
 	let hl = headerDetails[2].length;
-	line = parseInlineNodes(line);
 	return {parentNode: lastOpenedNode, nodeName: `h${hl}`, textContent: line.slice(hl + ph), children: []}
 }
 
@@ -40,7 +39,6 @@ function continueOpenedParagraph(lastOpenedNode: HtmlNode, line: string):boolean
 	}
 	let lastChild = lastOpenedContainer.children[lastOpenedContainer.children.length - 1];
 	if (lastChild && lastChild.nodeName === "paragraph" && !lastChild.closed) {
-		line = parseInlineNodes(line);
 		lastChild.textContent += line // paragraph continuation line
 		return true;
 	}
@@ -115,7 +113,7 @@ function addListItem(nodeName: string, lastOpenedNode: HtmlNode, line: string, m
 }
 
 // TODO: parse inlines, fix nested blockquotes bug, backslash escapes, proper tab to spaces conversion
-// find out ways to deal with tabs used for indentation
+// escape dangerous html
 function parseLine(line: string, lastOpenedNode: HtmlNode) {
 	if (line.search(/\S/) === -1) {
 		return closeNode(lastOpenedNode);
@@ -132,26 +130,23 @@ function parseLine(line: string, lastOpenedNode: HtmlNode) {
 			lastOpenedNode.closed = true;
 			lastOpenedNode = getValidOpenedAncestor(lastOpenedNode, markerPos);
 		}
+	}else if (nodeName === "plain text") {
+		const lineContinuedParagraph = continueOpenedParagraph(lastOpenedNode, line);
+		if (lineContinuedParagraph) {
+			return lastOpenedNode
+		}else if (lastOpenedNode.parentNode) {
+			lastOpenedNode = getValidOpenedAncestor(lastOpenedNode, markerPos); 
+		}
 	}
+	let nodeNewName = checkIfPartOfOtherNodeTypes(lastOpenedNode, markerPos);
+	nodeName = nodeNewName ? nodeNewName : nodeName;
 
 	if (nodeName === "hr") {
 		lastOpenedNode.children.push({parentNode: lastOpenedNode, nodeName, children: []})
 	}else if (nodeName === "header") {
 		lastOpenedNode.children.push(getHeaderNodeObj(line, lastOpenedNode))
 	}else if (nodeName === "plain text") {
-		const lineContinuedParagraph  = continueOpenedParagraph(lastOpenedNode, line);
-		if (lineContinuedParagraph) {
-			return lastOpenedNode
-		}else if (lastOpenedNode.parentNode) {
-			lastOpenedNode = getValidOpenedAncestor(lastOpenedNode, markerPos); 
-		}
-		let nodeNewName = checkIfPartOfOtherNodeTypes(lastOpenedNode, markerPos);
-		if (nodeNewName) {
-			nodeName = nodeNewName	
-		}else {
-			line = parseInlineNodes(line);
-			lastOpenedNode.children.push({parentNode: lastOpenedNode, nodeName: "paragraph", closed: false, textContent: line, children: []})
-		}
+		lastOpenedNode.children.push({parentNode: lastOpenedNode, nodeName: "paragraph", closed: false, textContent: line, children: []})
 	}else if (nodeName === "blockquote") {
 		lastOpenedNode.children.push(
 			{parentNode: lastOpenedNode, nodeName: "blockquote", closed: false, indentLevel:lastOpenedNode.indentLevel, children: []}
--- src/app/[slug]/parserHelper.tsx
+++ src/app/[slug]/parserHelper.tsx
@@ -1,10 +1,13 @@
 import type {HtmlNode} from "./page"
 import {lineIsHorizontalRule} from "./utilities"
 
-// human-friendly as well as browser-friendly html synatx generator
+// human-friendly as well as browser-friendly html syntax generator
 export function createInnerHtml(rootNode: HtmlNode, indentLevel: number):string {
 	let text = "";
 	const whiteSpace = ' '.repeat(indentLevel);
+	if (rootNode.nodeName === "paragraph" || (/h[1-6]/).test(rootNode.nodeName)) {
+		rootNode.textContent = parseInlineNodes(rootNode.textContent as string);
+	}
 	if (rootNode.nodeName === "html block") {
 		text = `${whiteSpace}${rootNode.textContent}\n`
 	}else if (rootNode.nodeName === "paragraph") {
@@ -42,7 +45,7 @@ export function getBlockNodes(line: string): [string, number] {
 	}else if ((/\s*#{1,6}\s/).test(line)) {
 		markerPos = line.indexOf('#')
 		nodeName = "header"
-	}else if ((/^\s*```/).test(line)) {
+	}else if ((/^\s*`{3,}[^`]*$/).test(line)) {
 		markerPos = line.indexOf('`');
 		nodeName = "fenced code";
 	}else if (lineIsHorizontalRule(line)){ // hr
@@ -51,7 +54,7 @@ export function getBlockNodes(line: string): [string, number] {
 	}else if ((markerPos = line.search(/<\/?(?:\w|\d)./)) !== -1) {
 		nodeName = "html block"; // possibly
 	}else {
-		let listMarkerDetails = (/(\s*)(\d{1,9}(?:\.|\)))(\s+)/).exec(line) || (/(\s*)(-|\+|\*)(\s+)/).exec(line);
+		let listMarkerDetails = (/^(\s*)(\d{1,9}(?:\.|\)))(\s+)/).exec(line) || (/^(\s*)(-|\+|\*)(\s+)/).exec(line);
 		if (listMarkerDetails) {
 			markerPos = listMarkerDetails[1].length;
 			if (("+-*").includes(listMarkerDetails[2])) {
@@ -67,10 +70,256 @@ export function getBlockNodes(line: string): [string, number] {
 }
 
 export function parseInlineNodes(line: string): string {
-	line = line.replaceAll(/!\[(.*?)\]\((.*?)\)/g, '<img src="$2" alt="$1">')
-	line = line.replaceAll(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
-	line = line.replaceAll(/(\*|_)(.+)?\1/g, '<em>$2</em>');
-	line = line.replaceAll(/(\*|_)+(.+)?\1/g, '<strong>$2</strong>');
-	line = line.replaceAll(/(`+)(.+)?\1/g, '<pre><code>$2</code></pre>');
-	return line;
+	let newLineTokens:string[] = []
+
+	let startIndex = 0;
+	while (true) {
+		let startToken = (/`+/).exec(line.slice(startIndex));
+		let endToken: any;
+
+		if (startToken) {
+			let endPattern = new RegExp("(?<!`)"+startToken[0]+"(?!`)");
+			endToken = endPattern.exec(line.slice(startIndex+startToken.index+startToken[0].length));
+			if (!endToken){
+				newLineTokens.push(line.slice(startIndex, startIndex+startToken.index+startToken[0].length))
+				startIndex = startIndex+startToken.index+startToken[0].length;
+				continue;
+			}
+		}else {
+			newLineTokens.push(line.slice(startIndex))
+			break;
+		}
+		if (startToken && endToken) {
+			let tempStartIndex = startIndex+startToken.index+startToken[0].length;
+			let codeSpan = line.slice(tempStartIndex, tempStartIndex + endToken.index)
+			codeSpan = codeSpan.replaceAll(/(?:\n|\r\n)/g, " "); // as per gfm markdown spec
+			if ((codeSpan[0] === codeSpan[codeSpan.length - 1]) && codeSpan[0] === ' ') {
+				codeSpan = codeSpan.slice(1, codeSpan.length - 1).replaceAll('&', '&amp;');
+			}
+			newLineTokens.push(line.slice(startIndex, startIndex+startToken.index))
+			newLineTokens.push(`<code>${codeSpan}</code>`)
+			startIndex = tempStartIndex + endToken.index + endToken[0].length;
+		}else continue; // execution should never reach here but allow
+	}
+
+	for (let i = 0; i < newLineTokens.length; i++) {
+		if (!newLineTokens[i].startsWith('<code')) {
+			const head = createLinkedListWith(newLineTokens[i])
+			newLineTokens[i] = parseLinksIfPresent(head);
+		}
+	}
+	return newLineTokens.join('');
 }
+
+interface Node {
+	content: string;
+	type: string;
+	closed: boolean;
+	next: Node|null;
+	prev: Node|null;
+}
+
+function createLinkedListWith(text: string) {
+	const head: Node = {content: "", type: "", closed: false, next: null, prev: null}
+	let currNode: Node = {content: "", type: "", closed: false, next: null, prev: head}
+	head.next = currNode;
+	let charIsEscaped = false
+
+	for (let i=0; i<text.length; i++) {
+		if (charIsEscaped && ("[]()\\").includes(text[i])) {
+			if (currNode.type !== "inline content") {
+				currNode.next = {content: text[i], type: "inline content", closed: true, next: null, prev: currNode};
+				currNode = currNode.next;
+			}
+			currNode.content += text[i];
+			charIsEscaped = false;
+		}else if (text[i] === '\\') {
+			charIsEscaped = true
+		}else if ((/\s/).test(text[i])) {
+			if (currNode.type !== "white space") {
+				currNode.next = {content: text[i], type: "white space", closed: true, next: null, prev: currNode};
+				currNode = currNode.next;
+			}else {
+				currNode.content += text[i];
+			}
+		}else if (("[()]").includes(text[i])) {
+			currNode.next = {content: text[i], type: "marker", closed: false, next: null, prev: currNode};
+			currNode = currNode.next;
+		}else if (currNode.type === "inline content") {
+			currNode.content += text[i];
+		}else {
+			currNode.next = {content: text[i], type: "inline content", closed: true, next: null, prev: currNode};
+			currNode = currNode.next;
+		}
+	}
+	return head;
+}
+
+function proper(titleText: string) {
+	let lastIndex = titleText.length - 1
+	if (titleText.includes('\n\n')) {
+		return false;
+	}else if (titleText[0] === "'" && titleText[lastIndex] === "'")  {
+		return true
+	}else if (titleText[0] === '"' && titleText[lastIndex] === '"') {
+		return true
+	}else if (titleText[0] === '(' && titleText[lastIndex] === ')') {
+		return true
+	}
+	return false;
+}
+
+
+function getLinkAttributes(startNode: Node) {
+	let partBeingProcessed = "link destination";
+	let linkDestination = "";
+	let linkTitle = "";
+	let unclosedParenthesis = 0
+	let currentNode = startNode.next as Node;
+
+	if (!currentNode) {
+		return null
+	}
+
+	while (true) {
+		if (partBeingProcessed === "link destination") {
+			if (currentNode.type === "white space") {
+				if (linkDestination && linkDestination[0] !== '<') {
+					partBeingProcessed = "link title"
+				}else if (linkDestination[0] === '<' && currentNode.content.includes('\n')) {
+					return null
+				}else if (!linkDestination && currentNode.next) {
+					currentNode = currentNode.next;
+					continue;
+				}else if (!currentNode.next) {
+					return null
+				}
+			}else if (currentNode.content === '(' && currentNode.type === "marker") {
+				unclosedParenthesis++;
+			}else if (currentNode.content === ')' && currentNode.type === "marker") {
+				if (unclosedParenthesis > 0) {
+					unclosedParenthesis--
+				}else {
+					return {destination: linkDestination, title: "", lastNodeWithAtrribute: currentNode}
+				}
+			}
+			linkDestination += currentNode.content
+		}else if (partBeingProcessed === "link title") {
+			if (!linkTitle && currentNode.type === "white space") {
+				if (currentNode.next) {
+					currentNode = currentNode.next
+					continue;
+				}else {
+					return null
+				}
+			}else if (!linkTitle && !("\"'(").includes(currentNode.content[0])) {
+				return null
+			}
+			linkTitle += currentNode.content;
+			if (proper(linkTitle)) {
+				partBeingProcessed = "attributes delimiter"
+			}
+		}else {
+			if (currentNode.content === ')' && currentNode.type === "marker") {
+				return {destination: linkDestination, title: linkTitle, lastNodeWithAtrribute: currentNode}
+			}else if (currentNode.type !== "white space") {
+				return null
+			}
+		}
+		if (!currentNode.next) {
+			return null
+		}
+		currentNode = currentNode.next;
+	}
+}
+
+
+function convertToText(startNode: Node, stopNode: Node) {
+	let currentNode = startNode
+	let text = ""
+
+	while (true) {
+		text += currentNode.content
+		if (currentNode === stopNode) {
+			break;
+		}else currentNode = currentNode.next as Node;
+	}
+	return text;
+}
+
+function parseLinksIfPresent(listHead: Node) {
+	let parsedText = "";
+	let linkTextEndNode;
+	let unBalancedBrackets = 0;
+	let openedLinkTextMarkers = []
+	let currentNode = listHead
+
+	while (true) {
+		if (!currentNode) {
+			return parsedText
+		}
+		if (currentNode.content === '[' && !currentNode.closed) {
+			openedLinkTextMarkers.push(currentNode);
+		}
+
+		if (openedLinkTextMarkers.length > 0) {
+			if (currentNode.content === ']' && !currentNode.closed) {
+				let lastIndex = openedLinkTextMarkers.length - 1;
+				currentNode.closed = true;
+				openedLinkTextMarkers[lastIndex].closed = true
+				if (currentNode.next && currentNode.next.content === '(' && currentNode.type === "marker") {
+					let linkAttributes = getLinkAttributes(currentNode.next)
+					if (!linkAttributes) {
+						if (openedLinkTextMarkers.length > 1) {
+							lastIndex--
+						}else parsedText += openedLinkTextMarkers[lastIndex].content; // so that it's content won't get omitted
+						currentNode = openedLinkTextMarkers[lastIndex];
+						openedLinkTextMarkers.pop();
+					}else {
+						let linkText = "";
+						if (openedLinkTextMarkers[lastIndex].next !== currentNode) { // prevents [] syntax scenarios
+							linkText = convertToText(openedLinkTextMarkers[lastIndex].next as Node, currentNode.prev as Node)
+						}
+						let textBefore = "";
+						if (openedLinkTextMarkers.length > 1) {
+							textBefore = convertToText(openedLinkTextMarkers[0], openedLinkTextMarkers[lastIndex].prev as Node);
+						}
+						openedLinkTextMarkers.forEach(markerNode => {markerNode.closed = true});
+						openedLinkTextMarkers = [];
+						parsedText += textBefore + generateLink(linkText, linkAttributes.destination, linkAttributes.title)
+						currentNode = linkAttributes.lastNodeWithAtrribute
+					}
+				}else if (openedLinkTextMarkers.length === 1) { // this is the only unbalanced brackets so far
+					let lastIndex = openedLinkTextMarkers.length - 1;
+					parsedText += convertToText(openedLinkTextMarkers[lastIndex], currentNode)
+					openedLinkTextMarkers.pop();
+				}
+			}
+		}else {
+			parsedText += currentNode.content;
+		}
+		currentNode = currentNode.next as Node;
+	}
+	return parsedText
+}
+
+function generateLink(linkText: string, destination: string, linkTitle: string): string {
+	let uri = destination;
+	if (uri[0] === '<' && uri[uri.length - 1] === '>') {
+		uri = uri.slice(1, uri.length-1);
+	}
+	linkText = linkText.replaceAll('\n', ' ');
+	if (linkTitle) { // link title
+		return `<a href="${uri}" title=${linkTitle}>${linkText}</a>`
+	}
+	return `<a href="${uri}">${linkText}</a>`
+}
+
+// INLINE NODES PARSING STRATEGY
+
+// BACKSLASH
+// Any ASCII punctuation character may be backslash-escaped:
+// \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~
+// <p>!&quot;#$%&amp;'()*+,-./:;&lt;=&gt;?@[\]^_`{|}~</p>
+// Backslashes before other characters are treated as literal backslashes
+// Backslash escapes do not work in code blocks, code spans, autolinks, or raw HTML but they work in all other contexts
